bb758d674caaf7c5a422e25070674eb930fc50b6
Debabrata Das <debabrata.dd@gmail.com>
2020-12-23 12:26:27 +0530

    Lazy loading singleton

--- singleton/Logger.cs
+++ singleton/Logger.cs
@@ -7,26 +7,28 @@ namespace design_patterns.singleton
     public sealed class Logger
     {
         private static int counter = 0;
-        private static readonly object obj = new object();
+        //private static readonly object obj = new object();
         private Logger() { counter++; }
 
-        private static Logger instance = null;
+        private static readonly Lazy<Logger> instance = new Lazy<Logger>(()=> new Logger());
         public static Logger GetInstance
         {
             get
             {
                 //double-checked locking
-                if (instance == null)
-                {
-                    lock (obj)
-                    {
-                        if (instance == null)
-                        {
-                            instance = new Logger();
-                        }
-                    }
-                }
-                return instance;
+                //if (instance == null)
+                //{
+                //    lock (obj)
+                //    {
+                //        if (instance == null)
+                //        {
+                //            instance = new Logger();
+                //        }
+                //    }
+                //}
+
+
+                return instance.Value;
             }
         }
 
